fb31a865c85390df8107f1f92d650ca7e7a7c6af
Alix <a.boufflers@it-students.fr>
2021-02-19 15:39:24 +0100

    :bug: fix repos migrations

--- testMyElephant/database/migrations/2021_02_19_131710_create_repos_table.php
+++ testMyElephant/database/migrations/2021_02_19_131710_create_repos_table.php
@@ -14,7 +14,7 @@ class CreateReposTable extends Migration
     public function up()
     {
         Schema::create('repos', function (Blueprint $table) {
-            $table->id("id");
+            $table->id();
             $table->string('name');
             $table->string('url');
             $table->string('scanRapport');
