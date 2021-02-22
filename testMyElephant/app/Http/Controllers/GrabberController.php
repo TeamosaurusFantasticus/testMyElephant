9007b9f4a24c8fd74685415046d07e168014c4d4
Le-TRex <clarembo@gmail.com>
2021-02-22 16:50:32 +0100

    :recycle: refactor some variables names and bond with RepositoryController's methods

--- testMyElephant/app/Http/Controllers/GrabberController.php
+++ testMyElephant/app/Http/Controllers/GrabberController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Cz\Git\GitRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Filesystem\Filesystem;
+use App\Http\Controllers\RepositoryController;
+
 class GrabberController extends Controller
 {
     public function getTheGrabberGit(){
@@ -15,8 +18,13 @@ class GrabberController extends Controller
     }
 
     public function cloneRepo(Request $request){
-        $path = "allRepo/".$request->namerepo;
-        $repo = GitRepository::cloneRepository($request->repo, $path);
+        $path = "allRepo/".$request->repositoryName;
+        $repo = GitRepository::cloneRepository($request->repositoryURL, $path);
+        if(!empty(Auth::user()->id)){
+            $newRepositoryController = new RepositoryController;
+            $newRepositoryController->storeRepository($request);
+            $newRepositoryController->showRepositories();
+        }
         return view("getTheGrabberGit");
     }
 
@@ -28,9 +36,7 @@ class GrabberController extends Controller
 
     public function deleteProject(Request $request)
     {
-
         $dirname = 'allRepo/' . $request->repoToDelete;
-
         exec("rm -rf $dirname");
     }
 }
