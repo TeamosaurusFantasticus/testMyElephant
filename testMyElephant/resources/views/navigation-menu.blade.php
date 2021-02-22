13e2f0adc073a474015e98a1c420a645a237cc74
Alix <a.boufflers@it-students.fr>
2021-02-22 17:00:04 +0100

    :lipstick: adding front of profil and dashboard pages

--- testMyElephant/resources/views/navigation-menu.blade.php
+++ testMyElephant/resources/views/navigation-menu.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                    <a href="/">
+                        <div class="logo_black"></div>
                     </a>
                 </div>
 
