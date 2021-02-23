19fd5e71692da3e026defd22accefc3b77761e6b
Alix <a.boufflers@it-students.fr>
2021-02-23 15:45:58 +0100

    :heavy_plus_sign: add cooki policy pop up

--- testMyElephant/resources/views/getTheGrabberGit.blade.php
+++ testMyElephant/resources/views/getTheGrabberGit.blade.php
@@ -7,6 +7,7 @@
     <title>testMyElephant</title>
     <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}" >
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" >
+    <link rel="stylesheet" type="text/css" href="{{asset("vendor/cookie-consent/css/cookie-consent.css")}}">
 </head>
 <body>
     <div class="home">
@@ -14,10 +15,9 @@
             <div class="leftSide">
             <div class="logo_homePage"></div>
             <div class="formSection">
-                <a href="{{route("getDeletter")}}">Suppression de repository</a>
                 <form class="formCloneRepo" method="post" action="{{route('cloneRepo')}}">
                     @csrf
-                    <h3 class="bold largeFont">Veuillez rentrer l'url de votre repository Github ainsi qu'un nom à lui donner !</h3>
+                    <h3 class="bold largeFont">Veuillez entrer l'url de votre repository Github ainsi qu'un nom à lui donner !</h3>
                     <input type="text" name="repo" placeholder="URL du repository à examiner">
                     <div class="line"></div>
                     <input  type="text" name="namerepo" placeholder="Nom à donner à votre repository">
