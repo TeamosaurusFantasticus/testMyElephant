9be00a353f61c4346ebf5efea9f7d0be82f5d9e9
Le-TRex <clarembo@gmail.com>
2021-02-25 22:30:32 +0100

    :sparkles: :construction: begin implementation of a repository scanning method looking for security issues, using ProgPilot library

--- testMyElephant/app/Http/Controllers/GrabberController.php
+++ testMyElephant/app/Http/Controllers/GrabberController.php
@@ -17,6 +17,7 @@ class GrabberController extends Controller
         return view("getTheGrabberGit");
     }
 
+// cloneRepo() clones a repository in a local directory named by the user then calls scanRepo()
     public function cloneRepo(Request $request){
         $path = "allRepo/".$request->repositoryName;
         $repo = GitRepository::cloneRepository($request->repositoryURL, $path);
@@ -25,14 +26,24 @@ class GrabberController extends Controller
             $newRepositoryController->storeRepository($request);
             $newRepositoryController->showRepositories();
         }
+
+        $this->scanRepo($path);
         return view("getTheGrabberGit");
     }
 
+// scanRepo() provides an array of all security issues found by ProgPilot
+    public function scanRepo($path){
+        exec("./../vendor/bin/progpilot $path", $output);
+        ddd($output);
+    }
+
+//TODO processing ProgPilot report
+//TODO delete repo after scanning
+//TODO create a class that calls every method needed for our repository scanning process called xxxService
 
     public function getDeletter(){
         return view('getDeletter');
     }
-
 
     public function deleteProject(Request $request)
     {
