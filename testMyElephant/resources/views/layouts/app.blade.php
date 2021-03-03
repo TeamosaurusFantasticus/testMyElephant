dbdc6a62a09cff5d8f2cd2dbc79450bf31fd9d5e
Alix <a.boufflers@it-students.fr>
2021-03-03 11:17:26 +0100

    :lipstick: add the favicon on all views + add full name of document on all views

--- testMyElephant/resources/views/layouts/app.blade.php
+++ testMyElephant/resources/views/layouts/app.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}">
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
