e79d64ae01cda8345c59b92187b59de25a107f63
Alix <a.boufflers@it-students.fr>
2021-03-04 15:47:30 +0100

    :lock: add verification for URL github or gitlab

---
+++ testMyElephant/app/Rules/isGit.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class isGit implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return preg_match('^((http|https)://)?(www[.])?(github|gitlab)+([.]com)+(/[a-zA-Z0-9(-|/|=|?)?]+)+(.git)^', $value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return "Veuillez rentrer une URL GitHub";
+    }
+}
